fbf9d34f7a58af9a540e1c9ba7c0d790ea843d20
Igor Polousov <igorpolousov@gmail.com>
2021-12-09 14:49:42 +0300

    + delete row

--- Milestone_Project19-21/NotesViewController.swift
+++ Milestone_Project19-21/NotesViewController.swift
@@ -70,6 +70,14 @@ class NotesViewController: UITableViewController, SendNoteToArray {
         }
     }
     
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            notes.remove(at: indexPath.row)
+        }
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    
     func sendNote(_ notes: [Note]) {
         self.notes = notes
     }
@@ -83,17 +91,9 @@ class NotesViewController: UITableViewController, SendNoteToArray {
     }
     */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+   
+   
+ 
 
     /*
     // Override to support rearranging the table view.
