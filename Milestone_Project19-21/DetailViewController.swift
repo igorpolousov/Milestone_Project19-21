ded8307089746d4cb10918bc922f8ea1d7698b2d
Igor Polousov <igorpolousov@gmail.com>
2021-12-14 17:27:54 +0300

    + buttons to toolbar Detail View Controller

--- Milestone_Project19-21/DetailViewController.swift
+++ Milestone_Project19-21/DetailViewController.swift
@@ -18,8 +18,23 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.toolbar.tintColor = .systemOrange
+        navigationController?.navigationBar.tintColor = .systemOrange
+        
+        let trashButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteNote))
+        let addNewNote = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(addNote))
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        toolbarItems = [trashButton,space, addNewNote]
+        navigationController?.isToolbarHidden = false
        
+    }
+    
+    @objc func deleteNote() {
+        
+    }
+    
+    @objc func addNote() {
+        
     }
     
 }
